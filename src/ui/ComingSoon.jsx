abb028b4b07b9b84f678c1a4bbd6505c6ea52111
ahmed5353 <ahmedreda5353@gmail.com>
2024-03-30 16:28:10 +0200

    fixed

--- src/ui/ComingSoon.jsx
+++ src/ui/ComingSoon.jsx
@@ -1,8 +1,8 @@
 function ComingSoon() {
   return (
     <div className="flex justify-center items-center h-96 ">
-      <h1 className="font-semibold text-lg">
-        Just a quick demo; responsive user interface will be available soon....
+      <h1 className="text-lg">
+        Just a quick demo; responsive UI will be available soon....
       </h1>
     </div>
   );
